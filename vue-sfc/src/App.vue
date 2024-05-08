9cc1569a25fc73db6ec5f3cbda62150fffbe9417
Feyfa <fisikamodern00@gmail.com>
2024-05-09 05:11:25 +0700

    belajar tentang vuejs sfc di folder vue-sfc

---
+++ vue-sfc/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <Student @response="showData" studentName="Andy">
+    ini adalah nilai slot dari parent
+  </Student> 
+  <br>
+
+  <Student @response="showData" studentName="Budi">
+    <strong>ini adalah nilai slot dari parent</strong>
+  </Student> 
+  <br>
+  
+  <Student @response="showData" studentName="Ani">
+  
+  </Student>
+
+  <p>
+    student yang dipilih adalah {{ selectedStudent }}
+  </p>
+</template>
+
+<!-- menggunakan teknik Option API -->
+<script>
+  import Student from './components/Student.vue';
+  
+  export default {
+    components : {
+      Student
+    },
+    data() {
+      return {
+        selectedStudent: ''
+      }
+    },
+    methods: {
+      showData(whatever) {
+        this.selectedStudent = whatever
+      }
+    },
+  }
+</script>
+
+
+<!-- menggunakan teknik Composition API -->
+<!-- <script setup>
+  import Student from './components/Student.vue';
+</script> -->
---
+++ vue-sfc/src/components/Student.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- >slot></slot> digunakan untuk menampilkan hasil dari child elemen <Student></Student/> -->
+    <!-- misal seperti ini <Student><strong>Halo Semuanya</strong></Student> -->
+    <!-- maka elemen dari <strong>Halo Semuanya</strong> akan masuk ke dalam slot -->
+    <!-- tetapi jika jika bentuknya hanya seperti ini <Student /> saja, maka menggunakan value default nya -->
+    <slot>INI ADALAH SLOT DEFAULT KARENA PARENT KOMPONEN TIDAK MEMBERIKAN DATA</slot> <br>
+    ini adalah data dari siswa bernama {{ studentName }} <button @click="showData">Show Detail</button> <br>
+</template>
+
+<script>
+    export default {
+        // props digunakann untuk mendapatkan value ketika elemen <Student studentName="Jidan" /> digunakan
+        // dengan cara seperti itu lah props digunakan
+        props: {
+            studentName: String
+        },
+
+        // emit itu intinya kita mempunya data di child, dalam hal ini adalah Student.vue, lalu akan dikirimkan ke parent nya yang menggunakan elemen <Student/>
+        // jadi saya ingin mengirimkan value dari child yaitu studentName yang dikirimkan ke parentnya/luar
+        emits: ['response'],
+        methods: {
+            showData() {
+                this.$emit('response', this.studentName);
+            }
+        },
+    }
+</script>
